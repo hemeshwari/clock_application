f48aa4f0ba511cccbfc3403bd8c40e41e291ee30
hemeshwari <nisha7086.nv@gmail.com>
2022-02-10 16:33:08 +0530

    second commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:clock_application/clock_view.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,9 +11,60 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+
     return Scaffold(
+      backgroundColor: const Color(0xFF2D2f41),
       body: Container(
-        color: const Color(0xFF2D2f41),
+        padding: const EdgeInsets.all(32),
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            const Text(
+              'Clock',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+            // ignore: prefer_const_constructors
+            SizedBox(
+              height: 32,
+            ),
+            const Text(
+              'Clock',
+              style: TextStyle(color: Colors.white, fontSize: 64),
+            ),
+            const Text(
+              'Clock',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+
+            const ClockView(),
+            const Text(
+              'Timezone',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+
+            Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                // ignore: prefer_const_constructors
+                Icon(
+                  Icons.language,
+                  color: Colors.white,
+                ),
+                // ignore: prefer_const_constructors
+                SizedBox(
+                  width: 16,
+                ),
+                const Text(
+                  'UTC',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
